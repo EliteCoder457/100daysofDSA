e266dae48a50367675f3d8d7baad3f346cd89314
EliteCoder457 <codercm123@gmail.com>
2026-02-10 08:23:47 +0530

    Implement function to reverse a string in place

---
+++ Day9Problem2.c
@@ -0,0 +1,15 @@
+//344. Reverse String
+void reverseString(char* s, int sSize) {
+    int left = 0;
+    int right = sSize - 1;
+    char temp;
+
+    while (left < right) {
+        temp = s[left];
+        s[left] = s[right];
+        s[right] = temp;
+
+        left++;
+        right--;
+    }
+}
