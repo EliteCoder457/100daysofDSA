6160a968085a2af13f49eecf9a2c338f39bfd568
EliteCoder457 <codercm123@gmail.com>
2026-04-05 10:44:40 +0530

    Create Day61Problem2.c

---
+++ Day61Problem2.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+
+void dfs(int** isConnected, int n, int city, int* visited) {
+    visited[city] = 1;
+
+    for (int i = 0; i < n; i++) {
+        if (isConnected[city][i] == 1 && !visited[i]) {
+            dfs(isConnected, n, i, visited);
+        }
+    }
+}
+
+int findCircleNum(int** isConnected, int isConnectedSize, int* isConnectedColSize) {
+    int n = isConnectedSize;
+    
+    int* visited = (int*)calloc(n, sizeof(int)); // initialize with 0
+    int provinces = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (!visited[i]) {
+            dfs(isConnected, n, i, visited);
+            provinces++;
+        }
+    }
+
+    free(visited);
+    return provinces;
+}
